429c992bc2dd2fdce988e1286e39d63a0d6ad7fc
Garapan Airdrop <alfirazahwaaqilla@gmail.com>
2025-10-31 17:38:41 +0700

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,37 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    winston.format.errors({ stack: true }),
+    winston.format.json()
+  ),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.printf(({ timestamp, level, message, ...meta }) => {
+          let metaStr = '';
+          if (Object.keys(meta).length > 0) {
+            metaStr = JSON.stringify(meta);
+          }
+          return `${timestamp} [${level}]: ${message} ${metaStr}`;
+        })
+      )
+    }),
+    new winston.transports.File({ 
+      filename: 'logs/error.log', 
+      level: 'error',
+      maxsize: 5242880,
+      maxFiles: 5
+    }),
+    new winston.transports.File({ 
+      filename: 'logs/combined.log',
+      maxsize: 5242880,
+      maxFiles: 5
+    })
+  ]
+});
+
+module.exports = logger;
